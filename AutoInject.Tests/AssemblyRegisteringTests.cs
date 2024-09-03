dc9fd23bd7bb3eba0ae17ab8ee2ad8f363134dbe
EdenBeavis <ebbeavis@gmail.com>
2024-09-03 19:34:46 +1000

    Added ability to register different assemblies
    Updated checkout to v3

---
+++ AutoInject.Tests/AssemblyRegisteringTests.cs
@@ -0,0 +1,86 @@
+﻿using AutoInject.Test.Library;
+using AutoInject.Test.Library2;
+using AutoInject.Test.Library2.FolderTest;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutoInject.Tests
+{
+    public class AssemblyRegisteringTests
+    {
+        private IServiceCollection _serviceCollection;
+
+        public AssemblyRegisteringTests()
+        {
+            _serviceCollection = new ServiceCollection();
+        }
+
+        [Fact]
+        public void ServiceProviderWillHaveRegisteredAssemblyService()
+        {
+            _serviceCollection = _serviceCollection.AutoInjectRegisterServices(typeof(ScopedTestInterface));
+            var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+            var testInterface = serviceProvider.GetService<ScopedTestInterface>();
+            Assert.NotNull(testInterface);
+        }
+
+        [Fact]
+        public void ServiceProviderWontHaveUnregisteredAssemblyService()
+        {
+            _serviceCollection = _serviceCollection.AutoInjectRegisterServices(typeof(ScopedTestInterface));
+            var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+            var testInterface = serviceProvider.GetService<RegisteredInterface>();
+            Assert.Null(testInterface);
+        }
+
+        [Fact]
+        public void ServiceProviderWontHaveUnregisteredAssemblyServiceButWillHaveRegisteredInterface()
+        {
+            _serviceCollection = _serviceCollection.AutoInjectRegisterServices(typeof(ScopedTestInterface));
+            var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+            var testInterface = serviceProvider.GetService<ScopedTestInterface>();
+            Assert.NotNull(testInterface);
+            var testInterface2 = serviceProvider.GetService<RegisteredInterface>();
+            Assert.Null(testInterface2);
+        }
+
+        [Fact]
+        public void ServiceProviderWillHaveInterfacesFromTwoAssemblies()
+        {
+            _serviceCollection = _serviceCollection.AutoInjectRegisterServices(typeof(ScopedTestInterface), typeof(RegisteredInterface));
+            var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+            var testInterface = serviceProvider.GetService<ScopedTestInterface>();
+            Assert.NotNull(testInterface);
+            var testInterface2 = serviceProvider.GetService<RegisteredInterface>();
+            Assert.NotNull(testInterface2);
+        }
+
+        [Fact]
+        public void ServiceProviderWillHaveMultipleInterfacesFromAnAssembly()
+        {
+            _serviceCollection = _serviceCollection.AutoInjectRegisterServices(typeof(ScopedTestInterface));
+            var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+            var testInterface = serviceProvider.GetService<ScopedTestInterface>();
+            Assert.NotNull(testInterface);
+            var testInterface2 = serviceProvider.GetService<TransientTestInterface>();
+            Assert.NotNull(testInterface2);
+            var testInterface3 = serviceProvider.GetService<SingletonTestInterface>();
+            Assert.NotNull(testInterface3);
+        }
+
+        [Fact]
+        public void ServiceProviderWillHaveInterfacesInFolders()
+        {
+            _serviceCollection = _serviceCollection.AutoInjectRegisterServices(typeof(RegisteredInterface));
+            var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+            var testInterface = serviceProvider.GetService<RegisteredInAFolderInterface>();
+            Assert.NotNull(testInterface);
+            Assert.True(testInterface.Test());
+        }
+    }
+}
