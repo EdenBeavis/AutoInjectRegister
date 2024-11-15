42adcea4f91f00442c0f5e08e5d0f464e7f0844b
EdenBeavis <ebbeavis@gmail.com>
2024-11-15 22:32:11 +1100

    Added exclusion tests

---
+++ AutoInject.Test.Library/ExclusionTestInterface.cs
@@ -0,0 +1,27 @@
+﻿using AutoInject.Attributes;
+
+namespace AutoInject.Test.Library
+{
+    internal interface ExclusionTestInterface
+    {
+        public string Test();
+    }
+
+    [AutoInjectTransient]
+    internal class ExclusionTestClass : ExclusionTestInterface
+    {
+        public string Test() => "a";
+    }
+
+    [AutoInjectTransient]
+    internal class ExclusionTest2Class : ExclusionTestInterface
+    {
+        public string Test() => "b";
+    }
+
+    [AutoInjectTransient]
+    internal class ExclusionTestClassOnly
+    {
+        public string TestClassOnly() => "c";
+    }
+}
